4b29b86a039956b40e28cb83d1cb71b51826d188
tim-meiwald-bluefruit <tim.meiwald@bluefruit.co.uk>
2023-10-02 17:50:58 +0100

    Some failures to be expected

--- tests/test_payroll_UK_validators.py
+++ tests/test_payroll_UK_validators.py
@@ -3,27 +3,27 @@ from xeroapi.payroll_UK_validators import PostEmployeeModel, PutEmployeeModel
 
 def test_post_employee_model_no_optional():
     payload = {
-        "Title": "Mr",
-        "FirstName": "Yoyo",
-        "LastName": "Dobbins",
-        "DateOfBirth": "2022-07-01",
-        "Gender": "M",
-        "AddressLine1": "1 Street",
-        "City": "Numenor",
-        "PostCode": "N190N7"
+        "title": "Mr",
+        "first_name": "Yoyo",
+        "last_name": "Dobbins",
+        "date_of_birth": "2022-07-01",
+        "gender": "M",
+        "address_line_1": "1 Street",
+        "city": "Numenor",
+        "post_code": "N190N7"
     }
     s = PostEmployeeModel(**payload)
 
 
 def test_put_employee_model_no_optional():
     payload = {
-        "Title": "Mr",
-        "FirstName": "Yoyo",
-        "LastName": "Dobbins",
-        "DateOfBirth": "2022-07-01",
-        "Gender": "M",
-        "AddressLine1": "1 Street",
-        "City": "Numenor",
-        "PostCode": "N190N7"
+        "title": "Mr",
+        "first_name": "Yoyo",
+        "last_name": "Dobbins",
+        "date_of_birth": "2022-07-01",
+        "gender": "M",
+        "address_line_1": "1 Street",
+        "city": "Numenor",
+        "post_code": "N190N7"
     }
     s = PutEmployeeModel(**payload)
